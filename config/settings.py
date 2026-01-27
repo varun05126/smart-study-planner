b9baf3b6c91f071abda20b4dc72c6016fec34bb4
varun05126 <malthumkarvarun@gmail.com>
2026-01-27 23:41:00 +0530

    Finalizing deployment setup

--- config/settings.py
+++ config/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # --------------------------------------------------
@@ -141,6 +142,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # --------------------------------------------------
 # DEFAULTS
